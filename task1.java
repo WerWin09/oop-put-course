b5e49dc29045a0dbc42b3c23a4074ddc16a08914
WerWin09 <115872102+WerWin09@users.noreply.github.com>
2022-10-15 14:57:11 +0200

    Update task1.java

--- task1.java
+++ task1.java
@@ -18,7 +18,9 @@ public class task1 {
         rectangle red = new rectangle ();
         red.side1=5;
         red.side2=10;
-        
+        System.out.println ("Rectangle: ");
+        System.out.println ("Side a: "+red.side1);
+        System.out.println ("Side b: "+red.side2);
         }
 
 }
