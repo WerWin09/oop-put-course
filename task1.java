608c8a37d25c451d985e32ca742e2767827df4da
WerWin09 <115872102+WerWin09@users.noreply.github.com>
2022-10-15 14:43:17 +0200

    Update task1.java

--- task1.java
+++ task1.java
@@ -1,22 +1,21 @@
 public class task1 {
     
-    public static class prostokat {
-    int bok1;
-    int bok2;
-    int polep;
-    int obwodp;
+    public static class rectangle {
+    int side1;
+    int side2;
+    int fieldR;
     }
 
 
-    public static class trojkat {
-    int podstwa;
-    int wysokosc;
-    int polet;
+    public static class triangle {
+    int side;
+    int hight;
+    int fieldT;
 
     }
 
     public static void main(String[] args) {
-        prostokat czerwony = new prostokat ();
+        rectangle red = new rectangle ();
         
 
 
