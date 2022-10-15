d08f8e4fefcaa6f0f531862e8b425e4c29b67dce
WerWin09 <115872102+WerWin09@users.noreply.github.com>
2022-10-15 13:49:06 +0200

    Create zadanie1.java

---
+++ zadanie1.java
@@ -0,0 +1,25 @@
+public class zadanie1 {
+    
+    public static class prostokat {
+    int bok1;
+    int bok2;
+    int polep;
+    int obwodp;
+    }
+
+
+    public static class trojkat {
+    int podstwa;
+    int wysokosc;
+    int polet;
+
+    }
+
+    public static void main(String[] args) {
+        prostokat czerwony = new prostokat ();
+        
+
+
+        }
+
+}
